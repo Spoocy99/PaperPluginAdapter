c94d762ec705a3b02351d66eacb62fa0557efa31
Spoocy99 <76441091+Spoocy99@users.noreply.github.com>
2026-09-06 20:02:08 +0200

    Added logger mode and refactored caller logic.

--- plugin-adapter/src/main/java/dev/spoocy/adapter/log/BukkitLogger.java
+++ plugin-adapter/src/main/java/dev/spoocy/adapter/log/BukkitLogger.java
@@ -7,103 +7,159 @@ import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Marker;
 
+
 /**
+ * Utility class to handle logging.
+ *
  * @author Spoocy99 | GitHub: Spoocy99
  */
-
 public class BukkitLogger {
 
-    private BukkitLogger() { }
+    private static Mode MODE = Mode.PLUGIN;
+
+    public static Mode getMode() {
+        return MODE;
+    }
+
+    public static void setMode(@NotNull Mode mode) {
+        BukkitLogger.MODE = mode;
+    }
 
     public static void trace(@NotNull Marker marker, @NotNull String message, final @NotNull Object... args) {
-        getLogger().trace(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).trace(marker, message, args);
     }
 
     public static void trace(@NotNull Marker marker, @NotNull Component message, final @NotNull Object... args) {
-        getLogger().trace(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).trace(marker, message, args);
     }
 
     public static void trace(@NotNull String message, final @NotNull Object... args) {
-        getLogger().trace(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).trace(caller, message, args);
     }
 
     public static void trace(@NotNull Component message, final @NotNull Object... args) {
-        getLogger().trace(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).trace(caller, message, args);
     }
 
     public static void debug(@NotNull Marker marker, @NotNull String message, final @NotNull Object... args) {
-        getLogger().debug(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).debug(marker, message, args);
     }
 
     public static void debug(@NotNull Marker marker, @NotNull Component message, final @NotNull Object... args) {
-        getLogger().debug(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).debug(marker, message, args);
     }
 
     public static void debug(@NotNull String message, final @NotNull Object... args) {
-        getLogger().debug(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).debug(caller, message, args);
     }
 
     public static void debug(@NotNull Component message, final @NotNull Object... args) {
-        getLogger().debug(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).debug(caller, message, args);
     }
 
     public static void info(@NotNull Marker marker, @NotNull String message, final @NotNull Object... args) {
-        getLogger().info(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).info(marker, message, args);
     }
 
     public static void info(@NotNull Marker marker, @NotNull Component message, final @NotNull Object... args) {
-        getLogger().info(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).info(marker, message, args);
     }
 
     public static void info(@NotNull String message, final @NotNull Object... args) {
-        getLogger().info(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).info(caller, message, args);
     }
 
     public static void info(@NotNull Component message, final @NotNull Object... args) {
-        getLogger().info(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).info(caller, message, args);
     }
 
     public static void warn(@NotNull Marker marker, @NotNull String message, final @NotNull Object... args) {
-        getLogger().warn(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).warn(marker, message, args);
     }
 
     public static void warn(@NotNull Marker marker, @NotNull Component message, final @NotNull Object... args) {
-        getLogger().warn(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).warn(marker, message, args);
     }
 
     public static void warn(@NotNull String message, final @NotNull Object... args) {
-        getLogger().warn(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).warn(caller, message, args);
     }
 
     public static void warn(@NotNull Component message, final @NotNull Object... args) {
-        getLogger().warn(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).warn(caller, message, args);
     }
 
     public static void error(@NotNull Marker marker, @NotNull String message, final @NotNull Object... args) {
-        getLogger().error(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).error(marker, message, args);
     }
 
     public static void error(@NotNull Marker marker, @NotNull Component message, final @NotNull Object... args) {
-        getLogger().error(marker, message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).error(marker, message, args);
     }
 
     public static void error(@NotNull String message, final @NotNull Object... args) {
-        getLogger().error(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).error(caller, message, args);
     }
 
     public static void error(@NotNull Component message, final @NotNull Object... args) {
-        getLogger().error(ClassFinder.caller(), message, args);
+        Class<?> caller = ClassFinder.caller();
+        getLogger(caller).error(caller, message, args);
     }
 
+    @NotNull
+    public static PluginLogger getLogger(@NotNull Class<?> caller) {
+        if(MODE == Mode.PLUGIN) {
+            return PluginAdapter.getInstance().logger();
+        }
 
-    public static PluginLogger getLogger() {
-        PluginAdapter plugin = PluginAdapter.getProvider(ClassFinder.caller(3));
+        PluginAdapter plugin = null;
+
+        try {
+            plugin = PluginAdapter.getProvider(caller);
+        } catch (Exception ignored) { }
 
         if(plugin == null) {
             return new PluginLoggerImpl(Bukkit.getServer().getLogger());
         }
 
         return plugin.logger();
+    }
+
+    public enum Mode {
+
+        /**
+         * Logger will be the providing logger of the class
+         */
+        PROVIDER,
+
+        /**
+         * Logger will always be the logger
+         * of the current {@link PluginAdapter} instance
+         */
+        PLUGIN
+    }
+
+    private BukkitLogger() {
+        throw new UnsupportedOperationException("Cannot instantiate.");
     }
 
 }
